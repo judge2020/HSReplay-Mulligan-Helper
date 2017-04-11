0e95493fd1744371e62c6ff13ee92870d343431c
judge2020 <hunterray55@hotmail.com>
2017-04-11 11:19:16 -0400

    Testing 1

---
+++ HSReplay Mulligan Helper/MulliganLogic.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Hearthstone_Deck_Tracker;
+using Hearthstone_Deck_Tracker.Enums;
+using HtmlAgilityPack;
+using System.Windows;
+using System.Web;
+using Hearthstone_Deck_Tracker.Importing;
+using Hearthstone_Deck_Tracker.Utility.Logging;
+
+namespace HSReplay_Mulligan_Helper
+{
+	public class MulliganLogic
+	{
+		public static MulliganLogic Instance => _instance ?? (_instance = new MulliganLogic());
+		private static MulliganLogic _instance;
+
+		public bool ShouldShow;
+		private HtmlDocument _doc;
+
+		internal void TurnStart(ActivePlayer player)
+		{
+			Instance.ShouldShow = false;
+		}
+
+		internal void GameStart()
+		{
+			Instance.ShouldShow = true;
+		}
+
+		internal void Mulligan(Hearthstone_Deck_Tracker.Hearthstone.Card f)
+		{
+			MessageBox.Show(f.EnglishText);
+		}
+
+		private string ActiveDeckUrl() => DeckList.Instance.ActiveDeck.Url;
+
+		private async Task<string> MulliganWinrate(string cardName)
+		{
+			try
+			{
+				if(_doc == null)
+					_doc = await ImportingHelper.GetHtmlDoc(ActiveDeckUrl());
+				
+				
+				var ournode = _doc.DocumentNode.SelectNodes("//span[@class=\"card-name\"]").First(x => x.InnerText == cardName);
+				return HttpUtility.HtmlDecode(ournode.SelectSingleNode("//ancestor::tr/td[@class=\"winrate-cell\"][1]").InnerText);
+			}
+			catch (Exception ex)
+			{
+				Log.Error("Unable to get Mulligan Winrate ->");
+				Log.Error(ex);
+				return "error";
+			}
+		}
+	}
+}
---
+++ HSReplay Mulligan Helper/MulliganPlugin.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Windows.Controls;
+using Hearthstone_Deck_Tracker.API;
+using Hearthstone_Deck_Tracker.Plugins;
+
+namespace HSReplay_Mulligan_Helper
+{
+    public class MulliganPlugin : IPlugin
+    {
+	    public void OnLoad()
+	    {
+		    GameEvents.OnTurnStart.Add(MulliganLogic.Instance.TurnStart);
+			GameEvents.OnGameStart.Add(MulliganLogic.Instance.GameStart);
+			GameEvents.OnPlayerMulligan.Add(MulliganLogic.Instance.Mulligan);
+	    }
+
+	    public void OnUnload()
+	    {
+		    
+	    }
+
+	    public void OnButtonPress()
+	    {
+		    
+	    }
+
+	    public void OnUpdate()
+	    {
+		    
+	    }
+
+	    public string Name => "HSReplay Mulligan Helper";
+	    public string Description => "Shows you the statistics of certain cards in your mulligan phase when the current deck was imported via HSReplay.\n\nMake sure the current active deck's URL is HSreplay to enable usage.";
+		public string ButtonText => "Settings (TBD)";
+		public string Author => "Judge2020";
+		public Version Version => new Version(0, 0, 1);
+	    public MenuItem MenuItem => null;
+    }
+}
